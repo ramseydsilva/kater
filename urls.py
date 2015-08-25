8bf7c9c97316b6c83cc57d180f3442d33924be93
metabolixx <youngtimesofindia@gmail.com>
2015-08-25 14:49:13 -0400

    Add robots.txt

--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ from django.contrib.sitemaps.views import sitemap
 from django.conf import settings
 from django.conf.urls import include, patterns
 from django.template.response import TemplateResponse
+from django.views.generic import TemplateView
 from sitemaps import sitemaps
 
 
@@ -14,6 +15,7 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('api.urls')),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     url(r'', include('home.urls')),
 ]
 
